b03cc22e9a02957b06a1949414dc02d273b40ab5
tkanchanawanchai6403 <tkanchanawanchai6403@an098046-ct103v.nvstu.edu>
2015-01-31 12:09:13 -0500

    initial

---
+++ OverLoadingDemo/src/Gps.java
@@ -0,0 +1,17 @@
+// change v1 do not commit
+// change v2 commit - initial version //master & head in the same location
+// change v3 commit
+// change v4 commit
+// change v5 commit - create feature branch
+public class Gps {
+	
+	public String getDirection() {
+		return "Default Location";
+	}
+	
+	public String getDirection(String address) {
+		return address + " Location";
+	}
+
+	
+}
